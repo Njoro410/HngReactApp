9704318729630692b7458ddbf8c426674c8cd34b
njoro410 <brian.njoroge@student.moringaschool.com>
2022-11-05 12:57:05 +0300

    style:styled the input fields in contact page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import Home from "./pages/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' exact element={<Home/>} />
+        <Route path='/'  element={<Home/>} />
         <Route path='/contact'  element={<Contact/>} />
       </Routes>
-      <Home />
+      <Footer/>
     </Router>
   );
 }
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React from "react";
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <div className="flex flex-col justify-center items-center gap-16 pt-16    max-w-[1440px] h-screen left-0 top-0">
+      <div className="">
+        <p className="font-inter">contact me</p>
+        <p className="text-[20px]">
+          Hi there, contact me to ask me about anything you have in mind.
+        </p>
 
-export default Contact
+        <form className="max-w-[600px] m-auto">
+          <div className="grid grid-cols-2 gap-2">
+            <section className="flex flex-col">
+              <label for="first_name">First name</label>
+              <input
+                className="border  p-3"
+                type="text"
+                placeholder="Enter your first name"
+              />
+            </section>
+            <section className="flex flex-col">
+              <label for="first_name">Last name</label>
+              <input
+                className="border  p-3"
+                type="text"
+                placeholder="Enter your last name"
+              />
+            </section>
+          </div>
+          <label for="first_name">Email</label>
+          <input
+            className="border  p-3 w-full "
+            type="email"
+            placeholder="yourname@email.com"
+          />
+          <label for="message">Message</label>
+          <textarea
+            className="border p-3 w-full "
+            cols="30"
+            rows="10"
+            placeholder="Send a message and i'll reply you as soon as possible"
+          ></textarea>
+          <button className="border  p-3 w-full mt-2">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
