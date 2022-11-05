1886ab1a8d7f77f9cef4cf313a39f60930da07b5
njoro410 <brian.njoroge@student.moringaschool.com>
2022-11-05 21:13:58 +0300

    feat:added scroll to top function

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
+import { useEffect } from "react";
 
 function App() {
+  function ScrollTop() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+    }, [pathname]);
+
+    return null;
+  }
   return (
     <Router>
+      <ScrollTop />
       <Routes>
         <Route path='/'  element={<Home/>} />
         <Route path='/contact'  element={<Contact/>} />
