027dae6192dd0eaf99cff9d49f907be2e18c1ad5
njoro410 <brian.njoroge@student.moringaschool.com>
2022-11-05 20:44:34 +0300

    feat:added appropriate id tags

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,22 +15,25 @@ const Contact = () => {
               <input
                 className="border py-3 px-4 gap-2  rounded-[8px] shadow-sm placeholder:font-inter placeholder:font-normal placeholder:text-base placeholder-[#667085]"
                 type="text"
+                id="first_name"
                 placeholder="Enter your first name"
               />
             </section>
             <section className="flex flex-col md:my-4">
-              <label htmlFor="first_name" className="font-inter not-italic font-medium text-sm leading-5 text-[#344054]">Last name</label>
+              <label htmlFor="last_name" className="font-inter not-italic font-medium text-sm leading-5 text-[#344054]">Last name</label>
               <input
                 className="border py-3 px-4 gap-2  rounded-[8px] shadow-sm placeholder:font-inter placeholder:font-normal placeholder:text-base placeholder-[#667085]"
                 type="text"
+                id="last_name"
                 placeholder="Enter your last name"
               />
             </section>
           </div>
-          <label htmlFor="first_name" className="my-4 font-inter not-italic font-medium text-sm leading-5 text-[#344054]">Email</label>
+          <label htmlFor="email" className="my-4 font-inter not-italic font-medium text-sm leading-5 text-[#344054]">Email</label>
           <input
             className="border p-3 w-full mb-3 rounded-[8px] shadow-sm placeholder:w-[320px] placeholder:h-[24px] placeholder:font-inter placeholder:font-normal placeholder:text-base placeholder-[#667085]"
             type="email"
+            id="email"
             placeholder="yourname@email.com"
           />
           <label htmlFor="message" className="font-inter not-italic font-medium text-sm leading-5 text-[#344054]">Message</label>
@@ -38,6 +41,7 @@ const Contact = () => {
             className="border p-3 w-full  rounded-[8px] shadow-sm placeholder:font-inter placeholder:font-normal placeholder:text-base placeholder-[#667085]"
             cols="30"
             rows="5"
+            id="message"
             placeholder="Send a message and i'll reply you as soon as possible"
           ></textarea>
           <section className="flex gap-2 my-4">
@@ -47,7 +51,7 @@ const Contact = () => {
               you.
             </p>
           </section>
-          <button className="border p-3 w-full mt-2 font-inter rounded-[8px] shadow-sm bg-[#1570EF] text-white">Send Message</button>
+          <button id="btn__submit" className="border p-3 w-full mt-2 font-inter rounded-[8px] shadow-sm bg-[#1570EF] text-white">Send Message</button>
         </form>
       </div>
     </div>
